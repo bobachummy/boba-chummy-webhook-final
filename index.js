232977102b73a03cb126e1e6270c331d9c425655
Maryam Yaro <maryamyaro@Maryams-MacBook-Pro.local>
2025-04-08 16:36:48 +0100

    Fix fetch import to use default export

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+const fetch = require('node-fetch').default;
 const app = express();
 app.use(bodyParser.json());
 
